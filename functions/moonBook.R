bc274e213cb6f2559163efee212ad1a0a8b36cb0
Peong Gang Park <111733814+P-G-Park@users.noreply.github.com>
2023-01-09 19:54:21 +0900

    Create moonBook.R

---
+++ functions/moonBook.R
@@ -0,0 +1,9 @@
+require(moonBook)
+
+Binomial_leave_only_one <- function(mytable){
+  suppressWarnings(myt <- mytable2df(mytable))
+  TRUE_num <- (1:nrow(myt))[str_detect(myt[[1]], 'TRUE')]
+  myt[[1]][TRUE_num] <- myt[[1]][TRUE_num-2]
+  myt <- myt[-c(TRUE_num-2, TRUE_num-1),]
+  return(myt)
+}

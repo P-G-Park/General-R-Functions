bee5b14dccd5e92824f8f7596ccf877736bd1e20
Peong Gang Park <111733814+P-G-Park@users.noreply.github.com>
2023-01-09 20:26:33 +0900

    Update moonBook.R

--- functions/moonBook.R
+++ functions/moonBook.R
@@ -4,7 +4,7 @@ Binomial_leave_only_one <- function(mytable){
   suppressWarnings(myt <- mytable2df(mytable))
   TRUE_num <- (1:nrow(myt))[str_detect(myt[[1]], 'TRUE')]
   myt[[1]][TRUE_num] <- myt[[1]][TRUE_num-2]
-  myt[[5]][TRUE_num] <- myt[[5]][TRUE_num-2]
+  myt[TRUE_num,ncol(myt)] <- myt[TRUE_num-2,ncol(myt)]
   myt <- myt[-c(TRUE_num-2, TRUE_num-1),]
   return(myt)
 }

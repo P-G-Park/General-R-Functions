41d2442946a49ad27c3ef6a2d0defdabbe826f6d
Peong Gang Park <111733814+P-G-Park@users.noreply.github.com>
2023-11-14 18:24:17 +0900

    Update moonBook.R

--- functions/moonBook.R
+++ functions/moonBook.R
@@ -1,6 +1,7 @@
 require(moonBook)
 
 Binomial_leave_only_one <- function(mytable){
+  require(dplyr)
   suppressWarnings(myt <- mytable2df(mytable))
   TRUE_num <- (1:nrow(myt))[str_detect(myt[[1]], 'TRUE')]
   myt[[1]][TRUE_num] <- myt[[1]][TRUE_num-2]
@@ -8,5 +9,9 @@ Binomial_leave_only_one <- function(mytable){
   myt <- myt[-c(TRUE_num-2, TRUE_num-1),]
   colnames(myt) <- str_c(colnames(myt), ' ', myt[1,])
   myt <- myt[-1,]
+  myt <- myt %>% mutate_all(str_replace_all, "\\[ ", "(")
+  myt <- myt %>% mutate_all(str_replace_all, "\\[", "(")
+  myt <- myt %>% mutate_all(str_replace_all, "\\]", ")")
+  myt <- myt %>% mutate_all(str_replace_all, ";", " — ")
   return(myt)
 }

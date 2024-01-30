5af80287185f8ae7b848388392d0a71749c44766
Peong Gang Park <111733814+P-G-Park@users.noreply.github.com>
2024-01-30 17:19:36 +0900

    Create SDS.R

---
+++ functions/SDS.R
@@ -0,0 +1,3 @@
+height_sds = function(height, M_height, L_height, S_height){((height/M_height)^L_height -1) / (L_height * S_height)}
+weight_sds = function(weight, M_weight, L_weight, S_weight){((weight/M_weight)^L_weight -1) / (L_weight * S_weight)}
+bmi_sds = function(bmi, M_bmi, L_bmi, S_bmi){((bmi/M_bmi)^L_bmi -1) / (L_bmi * S_bmi)}

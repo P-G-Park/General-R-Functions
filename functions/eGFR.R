3f4fbe1cb5628eb500d4b4a3d1c7336e1ac1c417
Peong Gang Park <111733814+P-G-Park@users.noreply.github.com>
2025-04-22 13:09:25 +0900

    Update eGFR.R

--- functions/eGFR.R
+++ functions/eGFR.R
@@ -5,45 +5,46 @@ require(tidyverse)
 u25_CysC <- function(CysC, age, sex){
   K = 0
   age <- floor(age)
-  if (age == 0) {return("NA")}
-  if (age > 25) {return("NA")}
-  if (age == 1 & sex == "m") {K = 74.8}
-  if (age == 2 & sex == "m") {K = 75.6}
-  if (age == 3 & sex == "m") {K = 76.5}
-  if (age == 4 & sex == "m") {K = 77.3}
-  if (age == 5 & sex == "m") {K = 78.2}
-  if (age == 6 & sex == "m") {K = 79.0}
-  if (age == 7 & sex == "m") {K = 79.9}
-  if (age == 8 & sex == "m") {K = 80.8}
-  if (age == 9 & sex == "m") {K = 81.7}
-  if (age == 10 & sex == "m") {K = 82.6}
-  if (age == 11 & sex == "m") {K = 83.5}
-  if (age == 12 & sex == "m") {K = 84.4}
-  if (age == 13 & sex == "m") {K = 85.3}
-  if (age == 14 & sex == "m") {K = 86.3}
-  if (age == 15 & sex == "m") {K = 87.2}
-  if (age == 16 & sex == "m") {K = 83.7}
-  if (age == 17 & sex == "m") {K = 80.4}
-  if (age >= 18 & age <= 25 & sex == "m") {K = 77.1}
-  if (age == 1 & sex == "f") {K = 76.5}
-  if (age == 2 & sex == "f") {K = 76.8}
-  if (age == 3 & sex == "f") {K = 77.1}
-  if (age == 4 & sex == "f") {K = 77.4}
-  if (age == 5 & sex == "f") {K = 77.7}
-  if (age == 6 & sex == "f") {K = 78.0}
-  if (age == 7 & sex == "f") {K = 78.3}
-  if (age == 8 & sex == "f") {K = 78.6}
-  if (age == 9 & sex == "f") {K = 78.9}
-  if (age == 10 & sex == "f") {K = 79.3}
-  if (age == 11 & sex == "f") {K = 79.6}
-  if (age == 12 & sex == "f") {K = 79.9}
-  if (age == 13 & sex == "f") {K = 77.8}
-  if (age == 14 & sex == "f") {K = 75.8}
-  if (age == 15 & sex == "f") {K = 73.8}
-  if (age == 16 & sex == "f") {K = 71.9}
-  if (age == 17 & sex == "f") {K = 70.0}
-  if (age >= 18 & age <= 25 & sex == "f") {K = 68.3}
-  egfr = K/CysC
+  K = case_when(
+    age == 1  & sex == "m" ~ 74.8,
+    age == 2  & sex == "m" ~ 75.6,
+    age == 3  & sex == "m" ~ 76.5,
+    age == 4  & sex == "m" ~ 77.3,
+    age == 5  & sex == "m" ~ 78.2,
+    age == 6  & sex == "m" ~ 79.0,
+    age == 7  & sex == "m" ~ 79.9,
+    age == 8  & sex == "m" ~ 80.8,
+    age == 9  & sex == "m" ~ 81.7,
+    age == 10 & sex == "m" ~ 82.6,
+    age == 11 & sex == "m" ~ 83.5,
+    age == 12 & sex == "m" ~ 84.4,
+    age == 13 & sex == "m" ~ 85.3,
+    age == 14 & sex == "m" ~ 86.3,
+    age == 15 & sex == "m" ~ 87.2,
+    age == 16 & sex == "m" ~ 83.7,
+    age == 17 & sex == "m" ~ 80.4,
+    age >= 18 & age <= 25 & sex == "m" ~ 77.1,
+    age == 1  & sex == "f" ~ 76.5,
+    age == 2  & sex == "f" ~ 76.8,
+    age == 3  & sex == "f" ~ 77.1,
+    age == 4  & sex == "f" ~ 77.4,
+    age == 5  & sex == "f" ~ 77.7,
+    age == 6  & sex == "f" ~ 78.0,
+    age == 7  & sex == "f" ~ 78.3,
+    age == 8  & sex == "f" ~ 78.6,
+    age == 9  & sex == "f" ~ 78.9,
+    age == 10 & sex == "f" ~ 79.3,
+    age == 11 & sex == "f" ~ 79.6,
+    age == 12 & sex == "f" ~ 79.9,
+    age == 13 & sex == "f" ~ 77.8,
+    age == 14 & sex == "f" ~ 75.8,
+    age == 15 & sex == "f" ~ 73.8,
+    age == 16 & sex == "f" ~ 71.9,
+    age == 17 & sex == "f" ~ 70.0,
+    age >= 18 & age <= 25 & sex == "f" ~ 68.3,
+    TRUE ~ NA_real_
+  )
+  egfr = K*0.01/CysC
   return(egfr)
 }
 #
@@ -52,49 +53,49 @@ u25_CysC <- function(CysC, age, sex){
 u25_Cr <-  function(Cr, age, height, sex){
   K = 0
   age <- floor(age)
-  if (age == 0) {return("NA")}
-  if (age > 25) {return("NA")}
-  if (!(sex %in% c("m","f"))) {return("NA")}
-  if (age == 1 & sex == "m") {K = 35.7}
-  if (age == 2 & sex == "m") {K = 36.0}
-  if (age == 3 & sex == "m") {K = 36.3}
-  if (age == 4 & sex == "m") {K = 36.6}
-  if (age == 5 & sex == "m") {K = 36.9}
-  if (age == 6 & sex == "m") {K = 37.2}
-  if (age == 7 & sex == "m") {K = 37.5}
-  if (age == 8 & sex == "m") {K = 37.8}
-  if (age == 9 & sex == "m") {K = 38.1}
-  if (age == 10 & sex == "m") {K = 38.4}
-  if (age == 11 & sex == "m") {K = 38.7}
-  if (age == 12 & sex == "m") {K = 39.0}
-  if (age == 13 & sex == "m") {K = 40.8}
-  if (age == 14 & sex == "m") {K = 42.6}
-  if (age == 15 & sex == "m") {K = 44.5}
-  if (age == 16 & sex == "m") {K = 46.5}
-  if (age == 17 & sex == "m") {K = 48.6}
-  if (age >= 18 & age <= 25 & sex == "m") {K = 50.8}
-  if (age == 1 & sex == "f") {K = 33.1}
-  if (age == 2 & sex == "f") {K = 33.3}
-  if (age == 3 & sex == "f") {K = 33.6}
-  if (age == 4 & sex == "f") {K = 33.9}
-  if (age == 5 & sex == "f") {K = 34.1}
-  if (age == 6 & sex == "f") {K = 34.4}
-  if (age == 7 & sex == "f") {K = 34.7}
-  if (age == 8 & sex == "f") {K = 35.0}
-  if (age == 9 & sex == "f") {K = 35.2}
-  if (age == 10 & sex == "f") {K = 35.5}
-  if (age == 11 & sex == "f") {K = 35.8}
-  if (age == 12 & sex == "f") {K = 36.1}
-  if (age == 13 & sex == "f") {K = 36.9}
-  if (age == 14 & sex == "f") {K = 37.8}
-  if (age == 15 & sex == "f") {K = 38.6}
-  if (age == 16 & sex == "f") {K = 39.5}
-  if (age == 17 & sex == "f") {K = 40.4}
-  if (age >= 18 & age <= 25 & sex == "f") {K = 41.4}
-  egfr = K*height/Cr
+  K = case_when(
+    age == 1  & sex == "m" ~ 35.7,
+    age == 2  & sex == "m" ~ 36.0,
+    age == 3  & sex == "m" ~ 36.3,
+    age == 4  & sex == "m" ~ 36.6,
+    age == 5  & sex == "m" ~ 36.9,
+    age == 6  & sex == "m" ~ 37.2,
+    age == 7  & sex == "m" ~ 37.5,
+    age == 8  & sex == "m" ~ 37.8,
+    age == 9  & sex == "m" ~ 38.1,
+    age == 10 & sex == "m" ~ 38.4,
+    age == 11 & sex == "m" ~ 38.7,
+    age == 12 & sex == "m" ~ 39.0,
+    age == 13 & sex == "m" ~ 40.8,
+    age == 14 & sex == "m" ~ 42.6,
+    age == 15 & sex == "m" ~ 44.5,
+    age == 16 & sex == "m" ~ 46.5,
+    age == 17 & sex == "m" ~ 48.6,
+    age >= 18 & age <= 25 & sex == "m" ~ 50.8,
+    age == 1  & sex == "f" ~ 33.1,
+    age == 2  & sex == "f" ~ 33.3,
+    age == 3  & sex == "f" ~ 33.6,
+    age == 4  & sex == "f" ~ 33.9,
+    age == 5  & sex == "f" ~ 34.1,
+    age == 6  & sex == "f" ~ 34.4,
+    age == 7  & sex == "f" ~ 34.7,
+    age == 8  & sex == "f" ~ 35.0,
+    age == 9  & sex == "f" ~ 35.2,
+    age == 10 & sex == "f" ~ 35.5,
+    age == 11 & sex == "f" ~ 35.8,
+    age == 12 & sex == "f" ~ 36.1,
+    age == 13 & sex == "f" ~ 36.9,
+    age == 14 & sex == "f" ~ 37.8,
+    age == 15 & sex == "f" ~ 38.6,
+    age == 16 & sex == "f" ~ 39.5,
+    age == 17 & sex == "f" ~ 40.4,
+    age >= 18 & age <= 25 & sex == "f" ~ 41.4,
+    TRUE ~ NA_real_
+  )
+  egfr = K*height*0.01/Cr
   return(egfr)
 }
-  
+
 
 fas_cr <- function(cr, sex, age){
   age = floor(age)
@@ -129,5 +130,4 @@ fas_cr <- function(cr, sex, age){
   )
   return(107.3 * q / cr)
 }
-
 

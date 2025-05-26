84eff11f9344c448cdd5d6b156bf587701105be7
Peong Gang Park <111733814+P-G-Park@users.noreply.github.com>
2025-05-26 17:10:27 +0900

    Update stats.R

--- functions/stats.R
+++ functions/stats.R
@@ -1,5 +1,17 @@
 from_fit_to_OR <- function(fit, digits = 2){
   exp(cbind(coef(fit), confint(fit))) %>% 
+    as.data.frame() %>% 
+    rownames_to_column("Risk Factor") %>% 
+    `colnames<-`(c('Risk Factor', 'aOR', 'lower', 'upper')) %>% 
+    mutate(aOR = sprintf(str_c('%.', digits, 'f'), aOR),
+           '95% CI' = str_c(sprintf(str_c('%.', digits, 'f'),lower), " - ", 
+                            sprintf(str_c('%.', digits, 'f'), upper), ""),
+           'aOR (95% CI)' = str_c(aOR, ' (',`95% CI`, ')')) %>% select(-3, -4) %>% 
+    bind_cols(`p value` = sprintf(str_c('%.', digits + 1, 'f'),summary(fit)$coefficients[, "Pr(>|z|)"] ))
+}
+
+from_fit_to_OR2 <- function(fit, digits = 2){
+  exp(cbind(coef(fit), confint(fit))) %>% 
   as.data.frame() %>% 
   rownames_to_column("Risk Factor") %>% 
   `colnames<-`(c('Risk Factor', 'aOR', 'lower', 'upper')) %>% 
